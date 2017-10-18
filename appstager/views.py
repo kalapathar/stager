c33ac0b8113493c68049040ae4c572ecbc9638c5
Hugo Valent <valent1@stolaf.edu>
2017-10-18 13:56:08 -0500

    render

--- appstager/views.py
+++ appstager/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the stager index.")
+	context = {}
+	return render(request, "appstager/base.html",context)
