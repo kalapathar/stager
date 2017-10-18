8ba1338f601c3a477bf0557941d6cd6dced40520
Deepak Shah <shah2@stolaf.edu>
2017-10-18 18:39:21 -0500

    Added a simple event model

--- appstager/models.py
+++ appstager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+	event_name=models.CharField(max_length=30)
+	# def __init__(self):
+	# 	self.event_name=event_name
+
+	def __str__(self):
+		return (self.event_name)
+
+	
---
+++ appstager/tests/test_models.py
@@ -0,0 +1,23 @@
+#from django.test import TestCase
+import unittest,random
+from faker import Faker 
+from appstager.models import Event 
+
+# A list that contains random events of sounds since Faker does not have anything as such
+event_list=['vibrate','ring','activate torch_light','play thunder_sound'] 
+
+
+class TestEvent(unittest.TestCase):
+
+	def setUp(self):
+		self.fake=Faker()
+		# self.event=Event(
+		# 	event_name=self.fake.first_name()
+		# 	)
+		self.event=random.choice(event_list)
+
+	def test_event_exists(self):
+		expected_event=str(self.event)
+		self.assertEqual(expected_event,str(self.event))
+
+
