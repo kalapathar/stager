a35aa23423fe79c72c70f891c1c85aef95eac74d
Deepak Shah <shah2@stolaf.edu>
2017-10-19 12:00:48 -0500

    Added Sequence

--- appstager/models.py
+++ appstager/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
 class Event(models.Model):
 	event_name=models.CharField(max_length=30)
 	# def __init__(self):
@@ -9,4 +13,20 @@ class Event(models.Model):
 	def __str__(self):
 		return (self.event_name)
 
-	
+
+class Sequence:
+    def __init__(self,Event):
+        self.sequence_list=list();
+        self.sequence_list.append(Event)
+    def add_sequence(self,Event):
+        self.sequence_list.append(Event)
+	def delete_sequence(self):
+        self.sequence_list=list()
+    def delete_event(self,Event):
+        self.sequence_list.remove(Event)
+    def delete_latest_event(self):
+        self.sequence_list=self.sequence_list[:-1]
+        
+    def display_sequence(self):
+        for i in self.sequence_list:
+            print (i)
