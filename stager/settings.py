7b543696af6d5da3a39d1ce8878702db4e159f99
Luke Zimmerman <zimmeluk14@gmail.com>
2017-11-01 18:48:27 -0500

    first deploy

--- stager/settings.py
+++ stager/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, '../appstager/static'),
 ]
 
 # Simplified static file serving.
